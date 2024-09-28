f5de4a34ba49894cd9890db09df06a53d78b1400
meenuga-kaveri59 <meenugakaveri59@gmail.com>
2024-09-28 18:48:32 +0530

    Add files via upload

---
+++ Clockwise.java
@@ -0,0 +1,41 @@
+public class Clockwise {
+        public static void main(String args[]){
+        int[][] arr ={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        transpose(arr);
+       } 
+       public static void transpose(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }  
+        for( int i =0;i<arr.length;i++){
+            int start =0;
+            int end=arr.length-1;
+            while(start<end){
+                int temp= arr[i][start];
+                arr[i][start]=arr[i][end];
+                arr[i][end]=temp;
+                start++;
+                end--;
+            }
+            
+        for( i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();        
+            
+        }
+    
+    
+       }  
+    }
+}
+
+
+    
+    
+
